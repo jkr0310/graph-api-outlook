615abbc4e6e5f0fba62b0225fa9810c176a07b6d
jkr0310 <jk.rajesh86@gmail.com>
2023-11-20 22:02:59 -0500

    Initial commit

---
+++ src/main/java/com/ls/reconsearch/ReconsearchApplication.java
@@ -0,0 +1,13 @@
+package com.ls.reconsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconsearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReconsearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ls/reconsearch/ReconsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ls.reconsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconsearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
